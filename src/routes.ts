96a7a66fbec897130c5308603686689fc51a751c
arthur <arturh07@gmail.com>
2022-05-30 21:16:00 -0300

    Third Class

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagControler } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
-const createUserController = new CreateUserController()
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagControler();
 
-router.post("/users/create", createUserController.handle)
+router.post("/users/create", createUserController.handle);
+router.post("/tags/create", ensureAdmin,createTagController.handle);
 
 export { router }
--- src/service/CreateUserService.ts
+++ src/service/CreateUserService.ts
@@ -5,10 +5,11 @@ interface IUserRequest {
     name: string;
     email: string;
     admin?: boolean;
+    password: string;
 }
 
 class CreateUserService {
-    async execute({ name, email, admin}: IUserRequest) {
+    async execute({ name, email, admin, password}: IUserRequest) {
         const usersRepository = getCustomRepository(UserRepositories);
 
         if (!email) {
